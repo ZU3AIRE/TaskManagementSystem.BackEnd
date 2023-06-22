d5f4a1e9a0f5b79e461a11db9af72cbae48cd61f
Zubair Jamil <92372994+ZU3AIRE@users.noreply.github.com>
2023-06-22 10:10:48 +0500

    Added Code For Data Seed

---
+++ TaskManagementSystem.Database/Migrations/20230622045319_data seeded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskManagementSystem.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class dataseeded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TaskStatuses",
+                columns: new[] { "TaskStatusID", "IsActive", "Status" },
+                values: new object[,]
+                {
+                    { 1, true, "Pending" },
+                    { 2, true, "Development" },
+                    { 3, true, "Closed" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TaskStatuses",
+                keyColumn: "TaskStatusID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TaskStatuses",
+                keyColumn: "TaskStatusID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TaskStatuses",
+                keyColumn: "TaskStatusID",
+                keyValue: 3);
+        }
+    }
+}
