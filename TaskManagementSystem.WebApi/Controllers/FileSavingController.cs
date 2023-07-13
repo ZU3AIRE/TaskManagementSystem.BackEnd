32fefc3f8b42035d914dcc709b6431c0a21365a2
Gullzaibkhan <ngullzaib9@gmail.com>
2023-07-13 11:41:44 +0500

    Error Fixed ByGullzaib

--- TaskManagementSystem.WebApi/Controllers/FileSavingController.cs
+++ TaskManagementSystem.WebApi/Controllers/FileSavingController.cs
@@ -41,8 +41,6 @@ namespace TaskManagementSystem.WebApi.Controllers
                 {
                     var fileName = GenerateUniqueFileName();
 
-                    if (fileUrl.StartsWith("data:"))
-                    {
                         var base64Data = fileUrl.Substring(fileUrl.IndexOf(',') + 1);
                         var fileBytes = Convert.FromBase64String(base64Data);
                         var extension = fileUrl.Substring(fileUrl.IndexOf('/') + 1, fileUrl.IndexOf(';') - fileUrl.IndexOf('/') - 1);
@@ -65,7 +63,6 @@ namespace TaskManagementSystem.WebApi.Controllers
                         await db.SaveChangesAsync();
 
                         return Ok(new { dbPath });
-                    }
                 }
 
                 return BadRequest();
@@ -83,10 +80,49 @@ namespace TaskManagementSystem.WebApi.Controllers
 
             return fileName;
         }
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<IActionResult> UploadBySystem()
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files.First();
+                var folderName = Path.Combine("wwwroot", "Uploads");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    var fileEntity = new FileSaving
+                    {
+                        Path = dbPath,
+                        FileName = fileName,
+                    };
+                    db.FileSavings.Add(fileEntity);
+                    await db.SaveChangesAsync();
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+
         [HttpGet]
         public IActionResult GetFileNames()
         {
-            var fileNames = db.FileSavings.Select(x => x.FileName).ToList();
+            var fileNames = db.FileSavings.Select(f => f.FileName).ToList();
             return Ok(fileNames);
         }
 
